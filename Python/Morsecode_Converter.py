f77dc58125db9c091b6ea1206002c6e45fb388c9
Vamshi-27 <vamshira18@outlook.com>
2024-09-24 21:33:47 +0530

    sync

---
+++ Python/Morsecode_Converter.py
@@ -0,0 +1,37 @@
+# Morse-Code Encryption and Decryption
+
+Morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....','I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----', ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-'}
+
+Morse_code_dict1 = {value: key for key, value in Morse_code_dict.items()}
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += Morse_code_dict[letter] + ' '
+        else:
+            cipher += ' '
+    return cipher
+
+def decrypt(message):
+    decipher = ''
+    code= message.split(' ')
+    for letter in code:
+        if letter != '':
+            decipher += Morse_code_dict1[letter]
+    return decipher
+
+def main():
+    n=input("Press e to encrypt and d to decrypt:").lower()
+    if n=='e':
+        message = input("Enter the message to encrypt: ").upper()
+        result = encrypt(message)
+        print (result)
+    elif n=='d':
+        message = input("Enter the message to decrypt: ")
+        result = decrypt(message)
+        print (result)
+    else:
+        print("Invalid Input")
+
+if __name__ == '__main__':
+    main()
